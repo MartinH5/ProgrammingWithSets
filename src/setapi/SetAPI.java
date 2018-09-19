cf9c6d49ab35066ac770b5e6c679b6d0a517cadf
Mart_ <Mart_@LAPTOP-84J82GVA>
2018-09-19 10:43:18 +0200

    Finalize Implementation

--- src/setapi/SetAPI.java
+++ src/setapi/SetAPI.java
@@ -5,13 +5,21 @@
  */
 package setapi;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  *
  * @author Mart_
  */
-public class SetAPI implements SetInterface{
+public class SetAPI implements SetInterface {
+
+    private int infiP = 999;
+    private int infiN = -999;
 
     @Override
     public boolean isMember(int m, Set<Integer> set) {
@@ -20,27 +28,57 @@ public class SetAPI implements SetInterface{
 
     @Override
     public Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Set<Integer> union = new HashSet<>();
+        union.addAll(set1);
+        union.addAll(set2);
+        return union;
     }
 
     @Override
     public Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Set<Integer> intersect = new HashSet<>();
+        for (Integer integer : set1) {
+            if (set2.contains(integer)) {
+                intersect.add(integer);
+            }
+        }
+        return intersect;
     }
 
     @Override
     public Set<Integer> difference(Set<Integer> set1, Set<Integer> set2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Set<Integer> diff = new HashSet<>();
+        for (Integer integer : set1) {
+            if (!set2.contains(integer)) {
+                diff.add(integer);
+            }
+        }
+        for (Integer integer : set2) {
+            if (!set1.contains(integer)) {
+                diff.add(integer);
+            }
+        }
+        return diff;
     }
 
     @Override
-    public Set<Integer> complement(Set<Integer> set) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Set<Integer> complement(List<Integer> set) {
+        Set<Integer> compl = new HashSet<>();
+        List sortedList = new ArrayList(set);
+        Collections.sort(sortedList);
+        int lowest = set.get(0);
+        int highest = set.get(set.size() - 1);
+        compl.add(lowest - 1);
+        compl.add(highest + 1);
+        for (int i = lowest; i < highest; i++) {
+            if (!set.contains(i)) {
+                compl.add(i);
+            }
+        }
+        compl.add(infiN);
+        compl.add(infiP);
+
+        return compl;
     }
 
-    
-    
-    
-    
-    
 }
